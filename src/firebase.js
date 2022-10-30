cee036f256551fcac9c9c66c45346b75b0f175bf
Aleksin Danila Sergeevich <danya09kz@gmail.com>
2022-10-30 16:07:11 +0600

    upd

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,13 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_KEY,
-  authDomain: "chat-ab746.firebaseapp.com",
-  projectId: "chat-ab746",
-  storageBucket: "chat-ab746.appspot.com",
-  messagingSenderId: "901216368405",
-  appId: "1:901216368405:web:8ec942ee51611df5c49b1c",
+	apiKey: "AIzaSyAAeAztf-l7U_ascoymkp_rd_1DDyg2Xps",
+	authDomain: "react-chat-app-466ed.firebaseapp.com",
+	projectId: "react-chat-app-466ed",
+	storageBucket: "react-chat-app-466ed.appspot.com",
+	messagingSenderId: "748065016067",
+	appId: "1:748065016067:web:960a8e984596af61620136",
+	measurementId: "G-HR8NMH9P8N"
 };
 
 // Initialize Firebase
